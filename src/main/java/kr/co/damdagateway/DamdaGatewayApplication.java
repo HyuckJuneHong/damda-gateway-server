0a5c7b49c0f56b34a9cd9120d6f51d2083816bd5
홍혁준 <jjj8337721@naver.com>
2023-02-02 23:20:05 +0900

    fix : gateway module name update

--- src/main/java/kr/co/damdagateway/DamdaGatewayApplication.java
+++ src/main/java/kr/co/damdagateway/DamdaGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DamdagatewayApplication {
+public class DamdaGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DamdagatewayApplication.class, args);
+        SpringApplication.run(DamdaGatewayApplication.class, args);
     }
 
 }
